09968ad788ca0e8dee1edc3991072b4c8669f83d
André Rodrigues <andre360.ars@gmail.com>
2021-12-29 16:24:48 -0300

    Requisito 2 - cria função que adiciona imagem na pagina

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const input = document.getElementById('text-input');
 const text = document.getElementById('meme-text');
+const file = document.getElementById('meme-insert')
 
 // Requisito 1
 function textPrint() {
@@ -7,3 +8,17 @@ function textPrint() {
 }
 
 input.addEventListener('keyup', textPrint)
+
+// Requisito 2
+// função feita com base nesse post https://stackoverflow.com/questions/22087076/how-to-make-a-simple-image-upload-using-javascript-html/45931408#45931408
+function imageUpload () {
+    if (this.files[0]) {
+        let img = document.getElementById('meme-image');
+        img.onload = () => {
+            URL.revokeObjectURL(img.src); 
+        }
+
+        img.src = URL.createObjectURL(this.files[0]); 
+    }
+}    
+file.addEventListener('change', imageUpload)
